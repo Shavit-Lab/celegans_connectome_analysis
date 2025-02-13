8528bca2bf77eb1071d8cb426a8f98e7d9de25ab
Thomas Athey <tom.l.athey@gmail.com>
2025-02-13 09:34:12 -0500

    test

---
+++ celegans_connectome_analysis/get_nx.py
@@ -0,0 +1,67 @@
+# get the directory of this file
+import os
+from pathlib import Path 
+import networkx as nx
+import pandas as pd
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+project_path = Path(dir_path).parent
+# get the directory of the project
+
+
+def get_adult_c_elegans_nx():
+    path_syncounts = project_path / 'data' / '41586_2021_3778_MOESM4_ESM.xlsx'
+
+    # read in the data
+    df = pd.read_excel(path_syncounts, sheet_name="Dataset8", header=2)
+    # remove first row
+    df = df.iloc[1:]
+    # remove the first column
+    df = df.iloc[:,1:]
+
+    #name the first column "neuron type"
+    df.columns = ["neuron type"] + list(df.columns[1:])
+    # name the second column "neuron name"
+    df = df.rename(columns={df.columns[1]: "neuron name"})
+
+    # if neuron type is nan, copy the previous neuron type
+    df["neuron type"] = df["neuron type"].fillna(method='ffill')
+
+    # check that the neuron names are unique
+    row_names = df["neuron name"]
+    col_names = df.columns[2:]
+    assert row_names.nunique() == len(row_names)
+    assert col_names.nunique() == len(col_names)
+
+    # for neuron type, map Sensory to Sens.
+    df["neuron type"] = df["neuron type"].str.replace("Sensory", "Sens.")
+    df["neuron type"] = df["neuron type"].str.replace("Motor", "Mot.")
+    df["neuron type"] = df["neuron type"].str.replace("Inter", "Inter.")
+    df["neuron type"] = df["neuron type"].str.replace("Modulatory", "Mod.")
+    df["neuron type"] = df["neuron type"].str.replace("Motor", "Mot.")
+
+
+
+    G = nx.DiGraph()
+
+    # add edges
+    for i, row in df.iterrows():
+        # iterate through the entries of row
+        post = row["neuron name"]
+        for pre, val in row.items():
+            if pre not in ["neuron type", "neuron name"] and val > 0:
+                G.add_edge(pre, post, weight=val)
+        
+
+
+    # add neuron types
+    for i, row in df.iterrows():
+        neuron_name = row["neuron name"]
+        neuron_type = row["neuron type"]
+        # if neuron name is not in the graph, add it
+        if neuron_name not in G.nodes:
+            G.add_node(neuron_name)
+        G.nodes[neuron_name]["neuron type"] = neuron_type
+
+    return G
+        
